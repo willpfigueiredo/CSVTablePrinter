72271521615db10f70aa1347ff049f2091c7b4c3
willpfigueiredo <willfigueiredo@gmail.com>
2022-03-25 23:47:08 -0300

    Adjust table printing to deal with double byte UTF8 chars

--- TablePrinter.cpp
+++ TablePrinter.cpp
@@ -6,7 +6,9 @@ void TablePrinter::printTable(CSVTable& table)
 	fillMaxLength(table);
 	for (int rowIndex = 0; rowIndex < table.getRowsCount(); ++rowIndex) {
 		for (int colIndex = 0; colIndex < table.getColumnCount(); ++colIndex) {
-			cout << left << setw(m_maxLength.at(colIndex)) << table.getCellValue(rowIndex, colIndex)<<"|";
+			//difference in lengths due to UTF8 double byte chars
+			unsigned int difference = table.getCellValue(rowIndex, colIndex).length() - realUTF8CharLength(table.getCellValue(rowIndex, colIndex));
+			cout << left << setw(m_maxLength.at(colIndex)+difference) << table.getCellValue(rowIndex, colIndex)<<"|";
 		}
 		cout << endl;
 	}
@@ -20,9 +22,24 @@ void TablePrinter::fillMaxLength(CSVTable& table)
 	//stores the maximum length for every column in m_maxLength
 	for (int rowIndex = 0; rowIndex < table.getRowsCount(); ++rowIndex) {
 		for (int colIndex = 0; colIndex < table.getColumnCount(); ++colIndex) {
-			if (table.getCellValue(rowIndex, colIndex).length() > m_maxLength[colIndex]) {
-				m_maxLength[colIndex] = table.getCellValue(rowIndex, colIndex).length();
+			if (realUTF8CharLength(table.getCellValue(rowIndex, colIndex)) > m_maxLength[colIndex]) {
+				m_maxLength[colIndex] = realUTF8CharLength(table.getCellValue(rowIndex, colIndex));
 			}
 		}
 	}
+}
+
+unsigned int TablePrinter::realUTF8CharLength(std::string& str)
+{
+	unsigned int strLen = str.length();
+	setlocale(LC_ALL, "en_US.utf8");
+	unsigned int index = 0;
+	const char* c_str = str.c_str();
+	unsigned int charCount = 0;
+	while (index < strLen)
+	{
+		index += mblen(&c_str[index], strLen - index);
+		charCount += 1;
+	}
+	return charCount;
 }
--- TablePrinter.hpp
+++ TablePrinter.hpp
@@ -11,9 +11,11 @@ public:
 	void printTable(CSVTable& table);
 
 private:
-	std::vector<int> m_maxLength;//stores the max length for each column
+	std::vector<unsigned int> m_maxLength;//stores the max length for each column
 
 	void fillMaxLength(CSVTable& table);
+
+	unsigned int realUTF8CharLength(std::string& str);
 
 };
 
